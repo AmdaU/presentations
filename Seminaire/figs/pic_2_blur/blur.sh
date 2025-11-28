cd061b4bc4d3a55c0bf87cdd1866c09990c764a2
AmdaU <jbat2000@gmail.com>
2025-11-28 13:54:07 -0500

    quick commit!

---
+++ Seminaire/figs/pic_2_blur/blur.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+magick how.png -region "65%x65%+%[fx:w*0.33]+%[fx:h*0.25]" -gaussian-blur 0x15 how_blur1.png
+magick how.png -region "32%x65%+0+%[fx:h*0.25]" -gaussian-blur 0x15 -region "35%x65%+%[fx:w*0.63]+%[fx:h*0.25]" -gaussian-blur 0x15 how_blur2.png
+magick how.png -region "63%x65%+0+%[fx:h*0.25]" -gaussian-blur 0x15 how_blur3.png
+magick how.png -region "97%x65%+0+%[fx:h*0.25]" -gaussian-blur 0x15 how_blur4.png
